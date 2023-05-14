c0082485c72f986dba131a06fa7a8dafe5cc88ae
SantiagoMtz25 <santimtzv01@gmail.com>
2023-05-14 13:00:32 -0600

    Lobby

--- auux/src/App.js
+++ auux/src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { Component, useState } from 'react';
 import Logo from './components/Logo';
 import Timer from './components/Timer';
 import Current from './components/Current';
 import List from './components/List';
 import Search from './components/Search';
+import Button from './components/Button';
+import InputField from './components/InputField';
 
 function App() {
   return (
     <div className="App">
+      <div className='Lobby'>
+        <Button /> 
+        <Button />
+      </div>
       <div className="Header">
         <Logo />
         <Timer />
